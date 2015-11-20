71ba67ea1316eefbcf1674b3463c2f07a6902be2
Alexander Gude <alex.public.account@gmail.com>
2015-11-20 15:13:06 -0800

    Add a very simply Python lexer based on `ast`

---
+++ src/utils/python_contentizer/lexer.py
@@ -0,0 +1,44 @@
+import ast
+
+
+class Lexer(ast.NodeVisitor):
+    """Parse a node from a AST and return a tuple of the content.
+
+    The returned tuple is of the form:
+
+        (line_number, node_type, name)
+
+    node_type can be either "Import" or "Call".
+
+    """
+
+    def visit_Import(self, node):
+        """Called for "import library" statements."""
+        items = []
+        for item in node.names:
+            items.append((node.lineno, "Import", item.name))
+        self.generic_visit(node)
+        return items
+
+    def visit_ImportFrom(self, node):
+        """Called for "from library import object" statements."""
+        self.generic_visit(node)
+        return [(node.lineno, "Import", node.module)]
+
+    def visit_Call(self, node):
+        """Called for function and method calls."""
+        # Some nodes have their name in the function object
+        try:
+            id = node.func.id
+        except AttributeError:
+            pass
+        # Others (those called as methods, or with a library name leading) have
+        # the name in the attr block
+        try:
+            id = node.func.value.id + '.' + node.func.attr
+        except AttributeError:
+            pass
+
+        self.generic_visit(node)
+
+        return [(node.lineno, "Call", id)]
